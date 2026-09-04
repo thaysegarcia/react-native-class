f9500176b009ed8ea11b7b7bdce4f0575bb8eea6
thaysegarcia <thaysegarcia@outlook.com>
2026-09-04 17:05:45 -0300

    add auth and tabs routes

---
+++ app/(auth)/sign-on.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const SignOn = () => {
+    return (
+        <View>
+            <Text>SignOn</Text>
+        </View>
+    )
+}
+export default SignOn
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import "@/global.css"
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function App() {
     return (
@@ -7,6 +8,14 @@ export default function App() {
             <Text className="text-xl font-bold text-blue-500">
                 Welcome to Nativewind!
             </Text>
+
+            <Link href="/(auth)/sign-in">Go to Sign In</Link>
+            <Link href="/(auth)/sign-on">Go to Sign On</Link>
+
+            <Link href= {{
+                pathname: "/subscriptions/[id]",
+                params: {id: "spotify"},
+            }}>Spotify Subscription</Link>
         </View>
     );
 }
