97e049b6d39f30aca5be548e5101e8ad0ca8de87
thaysegarcia <thaysegarcia@outlook.com>
2026-09-05 08:44:11 -0300

    feat: add index to the tabs directory

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import {Tabs} from "expo-router";
+
+const TabLayout = () => (
+    <Tabs screenOptions={{headerShown: false}}>
+        <Tabs.Screen name="index" options={{title: 'Home'}} />
+    </Tabs>
+)
+
+export default TabLayout
